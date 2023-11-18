a1d27d0f6d98add123732cc2da34f305c52f9945
Big X Mark <127086054+bigxmark@users.noreply.github.com>
2023-11-18 09:12:56 +0800

    fixed the unremoved debugger logs

--- app/api/concerns/route.ts
+++ app/api/concerns/route.ts
@@ -11,17 +11,15 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createConcernSchema.safeParse(body);
 
-  console.log('Im here1');
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 })
 
-  console.log('Im here');
   const createConcern = await prisma.concern.create({
     data: {
       title: body.title,
       description: body.description
     }
   })
-  console.log('Im here2');
+
   return NextResponse.json(createConcern, { status: 201 })
 }
