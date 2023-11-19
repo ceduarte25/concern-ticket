b4534c3af8792908c9811f9275a900e22a85630b
bigxmark <127086054+bigxmark@users.noreply.github.com>
2023-11-19 10:39:02 +0800

    configured default font in radix ui to next/font inter (#11)

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -9,22 +9,22 @@ export default function NavBar() {
   const path = usePathname();
 
   const links = [
-    { label: 'Home', href: '/'},
-    { label: 'Concerns', href: '/concerns'},
+    { label: 'Home', href: '/' },
+    { label: 'Concerns', href: '/concerns' },
   ]
 
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
       <Link href='/'><HiTicket /></Link>
       <ul className='flex space-x-6'>
-        {links.map(link => 
-        <Link key={link.href}
-        className={classNames({
-          'text-zinc-900': path === link.href,
-          'text-zinc-500': path !== link.href,
-          'hover:text-zinc-800 transition-colors': true
-        })}
-        href={link.href}>{link.label}</Link>)}
+        {links.map(link =>
+          <Link key={link.href}
+            className={classNames({
+              'text-zinc-900': path === link.href,
+              'text-zinc-500': path !== link.href,
+              'hover:text-zinc-800 transition-colors': true
+            })}
+            href={link.href}>{link.label}</Link>)}
       </ul>
     </nav>
   )
