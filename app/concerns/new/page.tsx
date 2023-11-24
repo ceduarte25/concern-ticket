aad1d6f9b540a5513fcfc319b48509658e1078b8
bigxmark <127086054+bigxmark@users.noreply.github.com>
2023-11-24 15:21:48 +0800

    created the client-side-validation for new concern form (#16)

--- app/concerns/new/page.tsx
+++ app/concerns/new/page.tsx
@@ -1,20 +1,22 @@
 'use client'
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { Controller, useForm } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createConcernSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-interface ConcernForm {
-  title: string;
-  description: string;
-}
+type ConcernForm = z.infer<typeof createConcernSchema>;
 
 export default function NewConcern() {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<ConcernForm>();
+  const { register, control, handleSubmit, formState: { errors } } = useForm<ConcernForm>({
+    resolver: zodResolver(createConcernSchema)
+  });
   const [error, setError] = useState('');
 
   return (
@@ -35,11 +37,13 @@ export default function NewConcern() {
         <TextField.Root>
           <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
+        {errors && <Text color='red' as='p'>{errors.title?.message}</Text>}
         <Controller
           name='description'
           control={control}
           render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
         />
+        {errors && <Text color='red' as='p'>{errors.description?.message}</Text>}
         <Button>Submit New Concern</Button>
       </form>
     </div>
