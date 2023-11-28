32fa79edfff42b3a4e1985f8e18f0d33cc921d0e
Big X Mark <127086054+bigxmark@users.noreply.github.com>
2023-11-28 11:20:16 +0800

    fixed markdown in details now showing

--- app/concerns/[id]/page.tsx
+++ app/concerns/[id]/page.tsx
@@ -2,6 +2,7 @@ import ConcernStatusBadge from "@/app/components/ConcernStatusBadge";
 import prisma from "@/prisma/client"
 import { Card, Flex, Heading } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
   params: { id: string }
@@ -21,8 +22,8 @@ export default async function ConcernDetailsPage({ params }: Props) {
         <ConcernStatusBadge status={concern.status} />
         <p>{concern.createdAt.toDateString()}</p>
       </Flex>
-      <Card>
-        <p>{concern.description}</p>
+      <Card className='prose' mt='4'>
+        <ReactMarkdown>{concern.description}</ReactMarkdown>
       </Card>
     </div>
   )
