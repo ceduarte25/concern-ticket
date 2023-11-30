114351550108e0d4205d5467a643a46f22007dd3
bigxmark <127086054+bigxmark@users.noreply.github.com>
2023-11-30 15:51:55 +0800

    created edit button (#36)

--- app/concerns/[id]/page.tsx
+++ app/concerns/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { ConcernStatusBadge } from "@/app/components";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading } from "@radix-ui/themes";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Box, Button, Card, Flex, Grid, Heading } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
 
@@ -16,15 +18,23 @@ export default async function ConcernDetailsPage({ params }: Props) {
   if (!concern) notFound();
 
   return (
-    <div>
-      <Heading>{concern.title}</Heading>
-      <Flex gap='3' my='2' >
-        <ConcernStatusBadge status={concern.status} />
-        <p>{concern.createdAt.toDateString()}</p>
-      </Flex>
-      <Card className='prose' mt='4'>
-        <ReactMarkdown>{concern.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: '1', md: '2' }} gap='5'>
+      <Box>
+        <Heading>{concern.title}</Heading>
+        <Flex gap='3' my='2' >
+          <ConcernStatusBadge status={concern.status} />
+          <p>{concern.createdAt.toDateString()}</p>
+        </Flex>
+        <Card className='prose' mt='4'>
+          <ReactMarkdown>{concern.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/concerns/${concern.id}/edit`}>Edit Button</Link>
+        </Button>
+      </Box>
+    </Grid>
   )
 }
