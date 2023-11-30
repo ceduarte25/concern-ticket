a7b76dc858457a1a136b7a068b18028fe7b001ae
bigxmark <127086054+bigxmark@users.noreply.github.com>
2023-11-30 19:23:25 +0800

    applied srp to concern details page (#38)

---
+++ app/concerns/[id]/ConcernDetails.tsx
@@ -0,0 +1,19 @@
+import { ConcernStatusBadge } from "@/app/components";
+import { Concern } from "@prisma/client";
+import { Card, Flex, Heading } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
+
+export default function ConcernDetails({ concern }: { concern: Concern }) {
+  return (
+    <>
+      <Heading>{concern.title}</Heading>
+      <Flex gap='3' my='2' >
+        <ConcernStatusBadge status={concern.status} />
+        <p>{concern.createdAt.toDateString()}</p>
+      </Flex>
+      <Card className='prose' mt='4'>
+        <ReactMarkdown>{concern.description}</ReactMarkdown>
+      </Card>
+    </>
+  )
+}
