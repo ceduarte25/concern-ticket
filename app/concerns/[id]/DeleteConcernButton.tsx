90b6c0784b2bcda217c8485675ce9e91b69e142c
Big X Mark <127086054+bigxmark@users.noreply.github.com>
2023-12-04 22:13:32 +0800

    added a delete confimation dialog

--- app/concerns/[id]/DeleteConcernButton.tsx
+++ app/concerns/[id]/DeleteConcernButton.tsx
@@ -1,7 +1,27 @@
-import { Button, Text } from "@radix-ui/themes";
+'use client'
+
+import { AlertDialog, Button, Flex, Text } from "@radix-ui/themes";
 
 export default function DeleteConcernButton({ concernId }: { concernId: number }) {
   return (
-    <Button color='red'><Text size={{ sm: '1', md: '2' }}>Delete Concern</Text></Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'><Text size={{ sm: '1', md: '2' }}>Delete Concern</Text></Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Delete Concern #{concernId}</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this concern? This action cannot be undone.
+        </AlertDialog.Description>
+        <Flex mt='4' gap='3'>
+          <AlertDialog.Cancel>
+            <Button color='gray' variant='soft'>Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color='red'>Delete</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
