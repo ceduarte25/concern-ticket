fa903efa6265d3708c57e00410c8413a8b4e27e9
bigxmark <127086054+bigxmark@users.noreply.github.com>
2023-12-07 08:23:08 +0800

    created login/logout authentication (#56)

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,15 @@
 'use client'
 
-import Link from 'next/link'
-import { HiTicket } from 'react-icons/hi2'
-import { usePathname } from 'next/navigation'
+import { Box } from '@radix-ui/themes'
 import classNames from 'classnames'
+import { useSession } from 'next-auth/react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { HiTicket } from 'react-icons/hi2'
 
 export default function NavBar() {
   const path = usePathname();
+  const { status, data: session } = useSession();
 
   const links = [
     { label: 'Home', href: '/' },
@@ -18,14 +21,20 @@ export default function NavBar() {
       <Link href='/'><HiTicket /></Link>
       <ul className='flex space-x-6'>
         {links.map(link =>
-          <Link key={link.href}
-            className={classNames({
-              'text-zinc-900': path === link.href,
-              'text-zinc-500': path !== link.href,
-              'hover:text-zinc-800 transition-colors': true
-            })}
-            href={link.href}>{link.label}</Link>)}
+          <li key={link.href}>
+            <Link
+              className={classNames({
+                'text-zinc-900': path === link.href,
+                'text-zinc-500': path !== link.href,
+                'hover:text-zinc-800 transition-colors': true
+              })}
+              href={link.href}>{link.label}</Link>
+          </li>)}
       </ul>
+      <Box>
+        {status === 'authenticated' && <Link href='/api/auth/signout'>Sign out</Link>}
+        {status === 'unauthenticated' && <Link href='/api/auth/signin'>Sign in</Link>}
+      </Box>
     </nav>
   )
 }
