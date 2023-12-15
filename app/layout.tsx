af96097bdb776d24711855bb8921a59d1a73c702
bigxmark <127086054+bigxmark@users.noreply.github.com>
2023-12-15 23:39:00 +0800

    refactored user fetching using react query (#69)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
-import '@radix-ui/themes/styles.css';
-import './theme-config.css';
+import { Container, Theme } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import NavBar from './NavBar'
-import { Container, Theme } from '@radix-ui/themes';
-import AuthProvider from './auth/Provider';
+import QueryClientProvider from './QueryClientProvider'
+import AuthProvider from './auth/Provider'
+import './globals.css'
+import './theme-config.css'
 
-const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter' })
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -20,16 +25,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.variable}>
-        <AuthProvider>
-          <Theme>
-            <NavBar />
-            <main className='p-5'>
-              <Container>{children}</Container>
-            </main>
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme>
+              <NavBar />
+              <main className='p-5'>
+                <Container>{children}</Container>
+              </main>
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   )
