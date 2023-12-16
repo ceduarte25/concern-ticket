0618ea7f7c9ed787510a114d2b87cdb1ebdfda7c
Big X Mark <127086054+bigxmark@users.noreply.github.com>
2023-12-16 15:47:50 +0800

    created an api to assign users to concern

--- app/api/concerns/[id]/route.ts
+++ app/api/concerns/[id]/route.ts
@@ -1,52 +1,64 @@
-import authOptions from "@/app/auth/authOptions";
-import { concernSchema } from "@/app/validationSchemas";
-import prisma from "@/prisma/client";
-import { getServerSession } from "next-auth";
-import { NextRequest, NextResponse } from "next/server";
+import authOptions from '@/app/auth/authOptions'
+import { patchConcernSchema } from '@/app/validationSchemas'
+import prisma from '@/prisma/client'
+import { getServerSession } from 'next-auth'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const session = await getServerSession(authOptions)
-  if (!session)
-    return NextResponse.json({}, { status: 401 })
+  if (!session) return NextResponse.json({}, { status: 401 })
 
   const body = await request.json()
-  const validation = concernSchema.safeParse(body);
-
+  const validation = patchConcernSchema.safeParse(body)
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 })
+
+  const { title, description, assignedToUserId } = body
+
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    })
+    if (!user)
+      return NextResponse.json({ error: 'Invalid User.' }, { status: 400 })
+  }
 
   const concern = await prisma.concern.findUnique({
     where: { id: parseInt(params.id) },
   })
-
   if (!concern)
     return NextResponse.json({ error: 'Invalid concern' }, { status: 404 })
 
-  const editConcern = await prisma.concern.update({
+  const patchedConcern = await prisma.concern.update({
     where: { id: concern.id },
     data: {
-      title: body.title,
-      description: body.description
-    }
+      title,
+      description,
+      assignedToUserId,
+    },
   })
 
-  return NextResponse.json(editConcern)
+  return NextResponse.json(patchedConcern)
 }
 
-export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const session = await getServerSession(authOptions)
-  if (!session)
-    return NextResponse.json({}, { status: 401 })
+  if (!session) return NextResponse.json({}, { status: 401 })
 
   const concern = await prisma.concern.findUnique({
-    where: { id: parseInt(params.id) }
+    where: { id: parseInt(params.id) },
   })
-
   if (!concern)
     return NextResponse.json({ error: 'Invalid concern' }, { status: 404 })
 
   await prisma.concern.delete({
-    where: { id: concern.id }
+    where: { id: concern.id },
   })
 
   return NextResponse.json({})
