dac67e6e4e7cd52c9d676b37a7250dffcc2e6ed2
bigxmark <127086054+bigxmark@users.noreply.github.com>
2023-12-19 18:50:59 +0800

    implemented pagination function (#80)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import { Pagination } from './components'
 
-export default function Home() {
-  return <Pagination itemCount={12} pageSize={5} currentPage={1} />
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string }
+}) {
+  return (
+    <Pagination
+      itemCount={27}
+      pageSize={5}
+      currentPage={parseInt(searchParams.page)}
+    />
+  )
 }
