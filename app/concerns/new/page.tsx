a0cb331a31bd3abc0b0bf31fc51c7a59d12ca8b2
Big X Mark <127086054+bigxmark@users.noreply.github.com>
2023-12-23 23:42:14 +0800

    immplemented dynamic metadata

--- app/concerns/new/page.tsx
+++ app/concerns/new/page.tsx
@@ -1,14 +1,24 @@
-import dynamic from "next/dynamic"
-import ConcernFormSkeleton from "./loading"
+import { Metadata } from 'next'
+import dynamic from 'next/dynamic'
+import ConcernFormSkeleton from './loading'
 
 const ConcernForm = dynamic(
   () => import('@/app/concerns/_components/ConcernForm'),
   {
     ssr: false,
-    loading: () => <ConcernFormSkeleton />
+    loading: () => <ConcernFormSkeleton />,
   }
 )
 
 export default function NewConcernPage() {
   return <ConcernForm />
+}
+
+export const metadata: Metadata = {
+  title: 'ConcernTicket - New Concern',
+  description: 'Submit a new concern',
+  creator: 'Christian Eduarte',
+  icons: {
+    icon: '/concernTicket.png',
+  },
 }
