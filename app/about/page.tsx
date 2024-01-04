430da38bdd8ced5280b68b40506db7ac996f1438
Christian Eduarte <127086054+ceduarte25@users.noreply.github.com>
2024-01-04 17:30:18 +0800

    created Technology section and made the aboutpage screen size responsize

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,14 @@
 import { Flex } from '@radix-ui/themes'
 import About from './About'
 import Author from './Author'
+import TechnologyUsed from './TechnologyUsed'
 
 export default function AboutPage() {
   return (
     <Flex direction='column' gap='5'>
       <About />
       <Author />
+      <TechnologyUsed />
     </Flex>
   )
 }
