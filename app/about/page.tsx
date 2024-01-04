093487dd3d4b8ccd9bd76eafb33fcb99caae507e
Christian Eduarte <127086054+ceduarte25@users.noreply.github.com>
2024-01-04 17:37:21 +0800

    implemented metadata in aboutpage

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,7 @@ import { Flex } from '@radix-ui/themes'
 import About from './About'
 import Author from './Author'
 import TechnologyUsed from './TechnologyUsed'
+import { Metadata } from 'next'
 
 export default function AboutPage() {
   return (
@@ -11,4 +12,14 @@ export default function AboutPage() {
       <TechnologyUsed />
     </Flex>
   )
+}
+
+export const metadata: Metadata = {
+  title: 'ConcernTicket - About',
+  description:
+    'View the description of ConcernTicket Web App with detailed Author and Technology information',
+  creator: 'Christian Eduarte',
+  icons: {
+    icon: '/concernTicket.png',
+  },
 }
